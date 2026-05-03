8d6ab95b910d9959404109971a27f01216164af1
Jan Konecny <jankonecny@microsoft.com>
2026-05-03 19:07:02 +0200

    Galerie pridana

--- js/main.js
+++ js/main.js
@@ -42,17 +42,7 @@
     });
 
 
-    // Portfolio isotope and filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-    });
-    $('#portfolio-flters li').on('click', function () {
-        $("#portfolio-flters li").removeClass('active');
-        $(this).addClass('active');
-
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
-    });
+    // Portfolio isotope and filter - handled by gallery.js
 
 
     // Testimonials carousel
